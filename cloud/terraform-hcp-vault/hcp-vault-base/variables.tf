1316aa6771e84afc621b2fe8dfdd7aeca3b90f24
Jane <janehojy@gmail.com>
2022-05-25 17:53:54 +0800

    Update variables.tf

--- cloud/terraform-hcp-vault/hcp-vault-base/variables.tf
+++ cloud/terraform-hcp-vault/hcp-vault-base/variables.tf
@@ -1,19 +1,19 @@
 variable "hvn_id" {
   description = "The ID of the HCP HVN."
   type        = string
-  default     = "learn-hcp-vault-hvn"
+  default     = "hcp-vault-hvn"
 }
 
 variable "cluster_id" {
   description = "The ID of the HCP Vault cluster."
   type        = string
-  default     = "learn-hcp-vault"
+  default     = "hcp-vault-private"
 }
 
 variable "region" {
   description = "The region of the HCP HVN and Vault cluster."
   type        = string
-  default     = "us-west-2"
+  default     = "ap-southeast-1"
 }
 
 variable "cloud_provider" {
